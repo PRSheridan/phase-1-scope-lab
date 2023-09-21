9f4a93abd8f2647d6ef67980a468965632d03e5c
Phil <philrsheridan@gmail.com>
2023-09-21 16:50:43 -0400

    passed 5 tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "john"
+
+function upperCaseCustomerName() { return customerName = customerName.toUpperCase(); }
+function setBestCustomer() { bestCustomer = "not bob"; }
+function overwriteBestCustomer() { return bestCustomer = "maybe bob"; }
+function changeLeastFavoriteCustomer() { return leastFavoriteCustomer = "jack"; }
